0e5b427ff7691d464dc46cdd50d4d3f130c28d7b
Markus Heidemann <heidemann@netzwerkplan.de>
2016-02-19 10:32:07 +0100

    Add GUI WPF for handyList

---
+++ GUI_WPF3/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace GUI_WPF3
+{
+   using System.Windows;
+
+   using nwp.marhei.mobilephoneLibary;
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void buttonAddMobilephone_Click(object sender, RoutedEventArgs e)
+      {
+         Mobilephone h = new Mobilephone
+         {
+            Model = textBoxModel.Text,
+            Price = double.Parse(textBoxPrice.Text),
+            Producer = textBoxProducer.Text,
+            SerialNumber = textBoxSerial_Number.Text
+         };
+         System.Console.WriteLine(h);
+
+      }
+   }
+}
